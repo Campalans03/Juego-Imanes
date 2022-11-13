4df8ca9c3c7b128b9ad9bd1d1ef95d8ffe9b35b3
Campalans03 <jancampalans@gmail.com>
2022-11-13 12:31:12 +0100

    Animacions + Tielmap + Caminar
    animacions caminar i idle del iman junt, creacio de un tilemap inteligent i creacio de un script per moure el personatge

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+
+    private Animator animator;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        animator.SetFloat("speed", Mathf.Abs(moveInput));
+    }
+
+    void Update()
+    {
+         if(moveInput > 0){
+            transform.eulerAngles = new Vector3(0,0,0);
+        }if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0,180,0);
+        }
+    }
+}
